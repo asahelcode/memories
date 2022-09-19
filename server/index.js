58628f0313b175d3f1e225be283f46b9e76c1eb7
asahelcode <hoyx0101@gmail.com>
2022-09-19 16:35:29 +0000

    initial create

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+
+app.use(cors());
+
+const CONNECTION_URL =
+  'mongodb+srv://henry:8a6o3dBJwWpY7XJU@memories.fsb0hdg.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(
+    app.listen(PORT, () => {
+      console.log(`Server running at port ${PORT}`);
+    })
+  )
+  .catch((error) => console.log(error.message));
